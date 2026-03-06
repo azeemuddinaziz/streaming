5955c380dbad215bf7b9b31edfbb465829328fdf
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2026-03-06 15:19:26 +0530

    feat: add maintenance mode HTML and script for enabling/disabling maintenance

---
+++ system/scripts/maint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FILE="/var/www/html/maintenance.on"
+HTML_PATH="/var/www/html/Maintenance.html"
+
+if [ "$1" == "on" ]; then
+    # Optional: Update the message if a second argument is provided
+    if [ ! -z "$2" ]; then
+        sudo sed -i "s|<p id=\"maint-msg\">.*</p>|<p id=\"maint-msg\">$2</p>|g" $HTML_PATH
+    fi
+    sudo touch $FILE
+    sudo systemctl reload nginx
+    echo "--- Maintenance Mode ENABLED ---"
+elif [ "$1" == "off" ]; then
+    sudo rm -f $FILE
+    sudo systemctl reload nginx
+    echo "--- Maintenance Mode DISABLED ---"
+else
+    echo "Usage: ./maint.sh [on|off] \"Optional Message\""
+fi
